275feaa4c6de65d3d4c99ec9a77b6da2f086f548
MJ <muriloaj@gmail.com>
2018-05-31 19:26:55 -0300

    Logging N Stop enpoint

--- src/main/java/com/github/muriloaj/hispark/HelloSpark.java
+++ src/main/java/com/github/muriloaj/hispark/HelloSpark.java
@@ -1,9 +1,30 @@
 package com.github.muriloaj.hispark;
 
-import static spark.Spark.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.stop;
 
 public class HelloSpark {
+
+
     public static void main(String[] args) {
+
+        Logger logger = LoggerFactory.getLogger(HelloSpark.class);
+        logger.info("Sparking engines......");
         get("/hello", (req, res) -> "Hello Spark");
+
+        get("/stop", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+
+            logger.info("BBye Baby John ......");
+            stop();
+            return "bye....";
+        });
     }
+
 }
